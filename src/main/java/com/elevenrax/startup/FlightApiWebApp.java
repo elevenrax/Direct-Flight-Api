d2ac9b3104451a491c91c3d2a6899a32d5bd4628
Nate <nate@11rax.com>
2018-02-23 16:03:02 +1100

    initialise springboot app

---
+++ src/main/java/com/elevenrax/startup/FlightApiWebApp.java
@@ -0,0 +1,13 @@
+package com.elevenrax.startup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightApiWebApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightApiWebApp.class, args);
+	}
+
+}
